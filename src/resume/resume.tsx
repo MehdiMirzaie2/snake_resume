1f0ffa3bc3459e3a0e7af4939a4e300cf237d252
Mehdi Mirzaie <mehdimirzaie18@gmail.com>
2025-10-02 16:40:54 +0930

    update resume

--- src/resume/resume.tsx
+++ src/resume/resume.tsx
@@ -1,4 +1,4 @@
-import "./resume.css";
+import './resume.css';
 
 export default function Resume() {
   return (
@@ -13,121 +13,247 @@ export default function Resume() {
             </a>
           </li>
           <li>
-            <a href="https://github.com/MehdiMirzaie2">github.com/MehdiMirzaie2</a>
+            <a href="https://github.com/MehdiMirzaie2">
+              github.com/MehdiMirzaie2
+            </a>
           </li>
           <li>
-            <a href="https://github.com/MehdiMirzaie2">linkedin.com/in/mehdi-mirzaie</a>
+            <a href="https://github.com/MehdiMirzaie2">
+              linkedin.com/in/mehdi-mirzaie
+            </a>
           </li>
         </ul>
 
         <h2>Education</h2>
         <h3>
-          <span>42 Adelaide Coding Academy, Australia </span> <span>Mar 2023 – Present</span>
+          <span>University of Adelaide, Australia </span>{' '}
+          <span>Feb 2025 – Dec 2026</span>
+        </h3>
+        <p>Bachelor of Computer Science</p>
+        <h3>
+          <span>42 Adelaide Coding Academy, Australia </span>{' '}
+          <span>Mar 2023 – Present</span>
         </h3>
         <p>
-          RNCP 6 Certificate Computer Science |{" "}
-          <a href="https://drive.google.com/file/d/1smaMioBc9xDKyWE8Ji3UP8SNQyi1c5fQ/view?usp=sharing">Transcript</a>
+          RNCP 6 Certificate Computer Science |{' '}
+          <a href="https://drive.google.com/file/d/1smaMioBc9xDKyWE8Ji3UP8SNQyi1c5fQ/view?usp=sharing">
+            Transcript
+          </a>
         </p>
+
+        <h2>Experience</h2>
+        <h3>
+          <span>
+            <a>Discovery Day Technology</a> | <i>Susquehanna</i>
+          </span>{' '}
+          <span>Sep 2025</span>
+        </h3>
         <ul>
-          <li>Description: Coding academy with a rigorous, peer-reviewed learning model</li>
+          <li>
+            Participated in technology workshops exploring trading
+            systems and algorithmic development.
+          </li>
+          <li>
+            Gained insights into high-frequency trading infrastructure
+            and financial technology applications.
+          </li>
         </ul>
 
         <h3>
-          <span>University of South Australia, Australia </span> <span>Jan 2022 – Jan 2023</span>
+          <span>
+            <a>Web Developer</a> | <i>Mercy Me Marketing</i>
+          </span>{' '}
+          <span>Jul 2025 – Present</span>
         </h3>
-        <p>Bachelor of Aviation (Pilot) | 6.5/7.0 GPA</p>
+        <ul>
+          <li>
+            Developing and maintaining client websites using
+            WordPress, Shopify and React.
+          </li>
+          <li>
+            Improved client website SEO ranking by performing
+            comprehensive site audits and optimizations.
+          </li>
+          <li>
+            Collaborated with designers and marketing teams to enhance
+            user experience and site performance.
+          </li>
+        </ul>
+
+        <h3>
+          <span>
+            <a>Web Developer</a> |{' '}
+            <i>
+              Competitive Programming Club of University of Adelaide
+            </i>
+          </span>{' '}
+          <span>Mar 2025 – Jul 2025</span>
+        </h3>
+
+        <h3>
+          <span>
+            <a>Programming Mentor</a> |{' '}
+            <i>42 Adelaide Coding Academy</i>
+          </span>{' '}
+          <span>Jun 2023 – Jan 2025</span>
+        </h3>
+        <ul>
+          <li>
+            Mentored 100+ students, providing coding guidance and
+            personalised support.
+          </li>
+        </ul>
 
         <h2>Projects</h2>
         <h3>
           <span>
-            <a href="https://github.com/MehdiMirzaie2/Pong-Chat-App">Online Pong/Chat App</a> |{" "}
-            <i>Django, React, PostgreSQL, Docker, Nginx</i>
-          </span>{" "}
+            <a href="https://github.com/MehdiMirzaie2/Pong-Chat-App">
+              Online Pong/Chat App
+            </a>{' '}
+            | <i>Django, React, PostgreSQL, Docker, Nginx</i>
+          </span>{' '}
           <span>May 2024 – Oct 2024</span>
         </h3>
         <ul>
           <li>
-            Directed a team of 5 to design and develop a full-stack web application featuring live chat and an
-            interactive Pong tournament system.
+            Directed a team of 5 to design and develop a full-stack
+            web application featuring live chat and an interactive
+            Pong tournament system.
           </li>
-          <li>Integrated WebSocket protocols for low-latency communication in live chat and game features.</li>
-          <li>Enhanced team collaboration and code quality through rigorous code reviews using Pull Requests.</li>
-          <li>Containerised environment using Docker and Nginx for seamless scalability and reliability</li>
+          <li>
+            Integrated WebSocket protocols for low-latency
+            communication in live chat and game features.
+          </li>
+          <li>
+            Enhanced team collaboration and code quality through
+            rigorous code reviews using Pull Requests.
+          </li>
+          <li>
+            Containerised environment using Docker and Nginx for
+            seamless scalability and reliability
+          </li>
         </ul>
 
         <h3>
           <span>
-            <a href="https://github.com/MehdiMirzaie2/42-webserv">Web Server</a> |{" "}
-            <i>C++, Python, Sqlite3, HTML, CSS</i>
-          </span>{" "}
+            <a href="https://github.com/MehdiMirzaie2/42-webserv">
+              Web Server
+            </a>{' '}
+            | <i>C++, Python, Sqlite3, HTML, CSS</i>
+          </span>{' '}
           <span>Jan 2024 – May 2024</span>
         </h3>
         <ul>
-          <li>Led a team of 3 to build a high-performance web server modeled after NGINX.</li>
           <li>
-            Implemented object-oriented programming (OOP) principles and leveraged multiprocessing to achieve low
-            latency and high throughput.
+            Led a team of 3 to build a high-performance web server
+            modeled after NGINX.
           </li>
           <li>
-            Conducted extensive benchmarking and performance tuning, achieving a 20% reduction in response times
-            compared to initial iterations.
+            Implemented object-oriented programming (OOP) principles
+            and leveraged multiprocessing to achieve low latency and
+            high throughput.
+          </li>
+          <li>
+            Conducted extensive benchmarking and performance tuning,
+            achieving a 20% reduction in response times compared to
+            initial iterations.
           </li>
         </ul>
 
         <h3>
           <span>
-            <a href="https://github.com/MehdiMirzaie2/minirt">Ray Tracing Engine</a> |{" "}
-            <i>C, Graphics, Linear Algebra</i>
-          </span>{" "}
+            <a href="https://github.com/MehdiMirzaie2/minirt">
+              Ray Tracing Engine
+            </a>{' '}
+            | <i>C, Graphics, Linear Algebra</i>
+          </span>{' '}
           <span>Oct 2023 – Dec 2023</span>
         </h3>
         <ul>
-          <li>Led a team of 3 to build a high-performance web server modeled after NGINX.</li>
           <li>
-            Implemented object-oriented programming (OOP) principles and leveraged multiprocessing to achieve low
-            latency and high throughput.
+            Led a team of 3 to build a high-performance web server
+            modeled after NGINX.
           </li>
           <li>
-            Conducted extensive benchmarking and performance tuning, achieving a 20% reduction in response times
-            compared to initial iterations.
+            Implemented object-oriented programming (OOP) principles
+            and leveraged multiprocessing to achieve low latency and
+            high throughput.
+          </li>
+          <li>
+            Conducted extensive benchmarking and performance tuning,
+            achieving a 20% reduction in response times compared to
+            initial iterations.
           </li>
         </ul>
 
-        <h2>Awards</h2>
-        <h3>
-          <span>Capture The Flag, Cyber Battle Australia </span> <span>Jul 2024</span>
+			  <h2>Awards</h2>
+			   <h3>
+          <span>Jane Street ETC </span>{' '}
+          <span>Mar 2025</span>
         </h3>
         <ul>
-          <li>Won 1st place in a CTF competition with 250 national contenders.</li>
+          <li>
+            Secured 3rd place in Jane Street’s competitive algorithmic trading competition.
+          </li>
         </ul>
         <h3>
-          <span>Mark Oliphant College, South Australia </span> <span>Dec 2021</span>
+          <span>Capture The Flag, Cyber Battle Australia </span>{' '}
+          <span>Jul 2024</span>
+        </h3>
+        <ul>
+          <li>
+            Won 1st place in a CTF competition with 250 national
+            contenders.
+          </li>
+        </ul>
+        <h3>
+          <span>Mark Oliphant College, South Australia </span>{' '}
+          <span>Dec 2021</span>
         </h3>
         <ul>
           <li>Awarded DUX/Valedictorian.</li>
         </ul>
 
         <h2>Volunteering</h2>
-        <h3>
-          <span>Programming Mentor, 42 Adelaide Coding Academy </span> <span>Jun 2023 - Present</span>
+        {/* <h3>
+          <span>Programming Mentor, 42 Adelaide Coding Academy </span>{' '}
+          <span>Jun 2023 - Present</span>
         </h3>
         <ul>
-          <li>Mentored 100+ students, providing coding guidance and personalised support.</li>
-          <li>Facilitated school group projects, fostering collaboration and teaching problem-solving strategies.</li>
-          <li>Established 80% exam pass rate through personalised support.</li>
-        </ul>
+          <li>
+            Mentored 100+ students, providing coding guidance and
+            personalised support.
+          </li>
+          <li>
+            Facilitated school group projects, fostering collaboration
+            and teaching problem-solving strategies.
+          </li>
+          <li>
+            Established 80% exam pass rate through personalised
+            support.
+          </li>
+        </ul> */}
         <h3>
-          <span>AdelaideSEC conference / AISA </span> <span>Aug 2024</span>
+          <span>AdelaideSEC conference / AISA </span>{' '}
+          <span>Aug 2024</span>
         </h3>
         <ul>
-          <li>Volunteered to assist and guide 600+ attendees, improving event satisfaction</li>
+          <li>
+            Volunteered to assist and guide 600+ attendees, improving
+            event satisfaction
+          </li>
         </ul>
 
         <h2>Technical Skills</h2>
         <ul>
-          <li>Languages: Python, C, C++, SQL (Postgres), TypeScript, HTML, CSS, Bootstrap 5</li>
+          <li>
+            Languages: Python, C, C++, SQL (Postgres), TypeScript,
+            HTML, CSS, Bootstrap 5
+          </li>
           <li>Frameworks: React, Django, WordPress, Reactstrap</li>
-          <li>Developer Tools: Git, Docker, VS Code, Virtual Box, UTM</li>
+          <li>
+            Developer Tools: Git, Docker, VS Code, Virtual Box, UTM
+          </li>
         </ul>
       </div>
     </div>
